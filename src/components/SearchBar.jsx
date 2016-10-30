fdfc926a6f89a788e503871df0ef5b064e14b18d
alinaish <alinaish@gmail.com>
2016-10-30 23:07:21 +0300

    save state

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props)) {
+    super(props);
+    this.state = {
+      searchTerm = '';
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  }
+
   render() {
-    return <input />;
+    return <input onChange={this.handleInputChange} />;
   }
 }
 
