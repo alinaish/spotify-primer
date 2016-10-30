aab498745035f139de152496455e4fc36d31ae16
alinaish <alinaish@gmail.com>
2016-10-30 18:15:57 +0300

    add linter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Greeting from './greetings';
-
-ReactDOM.render(
-  <Greeting name="Alina"/>,
-  document.getElementById('container')
-);
+const foo = 'bar';
