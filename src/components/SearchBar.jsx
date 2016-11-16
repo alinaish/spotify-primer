a206d77529a10f5a7d52d25162255c118c56d86a
alinaish <alinaish@gmail.com>
2016-11-16 23:35:33 +0300

    show albums

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  constructor(props)) {
+  constructor(props) {
     super(props);
     this.state = {
-      searchTerm = '';
+      searchTerm: ''
     };
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   handleInputChange(event) {
@@ -15,9 +16,19 @@ class SearchBar extends React.Component {
     });
   }
 
+  handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      this.props.getAlbums(this.state.searchTerm);
+    }
+  }
+
   render() {
-    return <input onChange={this.handleInputChange} />;
+    return <input onChange={this.handleInputChange} onKeyPress={this.handleKeyPress}/>;
   }
 }
+
+SearchBar.propTypes = {
+  getAlbums: React.PropTypes.func.isRequired
+};
 
 export default SearchBar;
